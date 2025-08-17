669830c7cb9256c4ccd99c6435df10d2af390a58
ahlamdev <ahlam44tm3@gmail.com>
2025-08-17 05:25:31 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner(0);
+   
     
     
     // Initiate the wowjs
@@ -245,4 +237,5 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 
 })(jQuery);
+
 
